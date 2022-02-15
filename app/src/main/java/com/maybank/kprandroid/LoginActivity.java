80a3f9d32a0cab242c5d41ed5d9bf0000192018a
septiangirendra <septiangirendra@gmail.com>
2022-02-15 17:46:56 +0700

    Form Login - Septian

--- app/src/main/java/com/maybank/kprandroid/LoginActivity.java
+++ app/src/main/java/com/maybank/kprandroid/LoginActivity.java
@@ -2,13 +2,27 @@ package com.maybank.kprandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
     }
 }
