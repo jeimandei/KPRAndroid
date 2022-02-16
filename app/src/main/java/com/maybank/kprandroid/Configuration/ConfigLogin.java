328aab1ea23b3e4fbd80e7a0154e338548aad3f3
septiangirendra <septiangirendra@gmail.com>
2022-02-16 13:45:20 +0700

    Delete Schedule - Septian

--- app/src/main/java/com/maybank/kprandroid/Configuration/ConfigLogin.java
+++ app/src/main/java/com/maybank/kprandroid/Configuration/ConfigLogin.java
@@ -2,7 +2,7 @@ package com.maybank.kprandroid.Configuration;
 
 public class ConfigLogin {
 
-    public static final String URL_GET_LOGIN= "http://192.168.30.26/kpr/login/proses_login.php";
+    public static final String URL_GET_LOGIN= "http://192.168.31.103/kpr/login/proses_login.php";
     public static final String TAG_JSON_ARRAY_LOGIN = "result";
     public static final String TAG_LOGIN_EMP_ID = "id_emp";
 
