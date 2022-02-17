4e3984aa46f5c19d895d2b930a22b770febb9cd1
Jeremy Matthew M <22652262+jeimandei@users.noreply.github.com>
2022-02-17 13:02:45 +0700

    Manager Approval Layout - jeimandei

---
+++ app/src/main/java/com/maybank/kprandroid/Configuration/ConfigManager.java
@@ -0,0 +1,35 @@
+package com.maybank.kprandroid.Configuration;
+
+public class ConfigManager {
+    public static final String IP = "https://kprandroid.000webhostapp.com/";
+    // url dimana web API berada
+    public static final String URL_GET_ALL_MANAGER = IP + "kpr/approval/lihat_customer_all.php";
+    public static final String URL_GET_DETAIL_CUSTOMER = IP + "kpr/customer/tampilCustomer.php?id_nsb=";
+    public static final String URL_ADD_CUSTOMER = IP + "kpr/customer/tambahCustomer.php";
+    public static final String URL_UPDATE_CUSTOMER = IP + "kpr/customer/updateCustomer.php";
+    public static final String URL_DELETE_CUSTOMER = IP + "kpr/customer/hapusCustomer.php?id=";
+
+    // key and value JSON yang muncul di browser
+    public static final String KEY_CST_ID = "id_nsb";
+    public static final String KEY_CST_NAME = "nama_nsb";
+    public static final String KEY_CST_KTP = "ktp_nsb";
+    public static final String KEY_CST_TMPT_LAHIR = "tmp_lahir_nsb";
+    public static final String KEY_CST_TGL_LAHIR = "tgl_lahir_nsb";
+    public static final String KEY_CST_ALAMAT = "alamat_nsb";
+    public static final String KEY_CST_HP = "hp_nsb";
+    public static final String KEY_CST_ID_EMP = "id_emp";
+
+    // flag JSON
+    public static final String TAG_JSON_ARRAY = "result";
+    public static final String TAG_JSON_CST_ID = "id_nsb";
+    public static final String TAG_JSON_MNG_NAME = "nama_nsb";
+    public static final String TAG_JSON_CST_KTP = "ktp_nsb";
+    public static final String TAG_JSON_CST_HP = "hp_nsb";
+    public static final String TAG_JSON_CST_TMPT_LAHIR = "tmpt_lahir_nsb";
+    public static final String TAG_JSON_CST_TGL_LAHIR = "tgl_lahir_nsb";
+    public static final String TAG_JSON_CST_ALAMAT = "alamat_nsb";
+    public static final String TAG_JSON_MNG_NAME_EMP = "nama_emp";
+
+    // variabel ID peserta
+    public static final String NSB_ID = "id_nsb";
+}
--- app/src/main/java/com/maybank/kprandroid/Manager/ManagerFragment.java
+++ app/src/main/java/com/maybank/kprandroid/Manager/ManagerFragment.java
@@ -1,66 +1,182 @@
 package com.maybank.kprandroid.Manager;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.maybank.kprandroid.Configuration.ConfigCustomer;
+import com.maybank.kprandroid.Configuration.ConfigManager;
+import com.maybank.kprandroid.Customer.DetailCustomerFragment;
+import com.maybank.kprandroid.HttpHandler;
 import com.maybank.kprandroid.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ManagerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class ManagerFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String JSON_STRING;
+    private ViewGroup viewGroup;
+    private ListView listView;
+    String id_emp;
 
     public ManagerFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ManagerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ManagerFragment newInstance(String param1, String param2) {
         ManagerFragment fragment = new ManagerFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manager, container, false);
+        viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_manager, container, false);
+
+        listView = viewGroup.findViewById(R.id.lv_manager);
+
+        getJSON();
+
+
+        return viewGroup;
+    }
+
+
+    private void getJSON() {
+        class GetJSON extends AsyncTask<Void, Void, String> {
+            ProgressDialog progressDialog;
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog = ProgressDialog.show(getContext(), "Getting Data", "Please wait...", false, false);
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                HttpHandler handler = new HttpHandler();
+                String result = handler.sendGetResp(ConfigManager.URL_GET_ALL_MANAGER);
+                Log.d("GetData", result);
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        progressDialog.dismiss();
+                    }
+                }, 1000);
+
+                JSON_STRING = s;
+                Log.d("Data_JSON", JSON_STRING);
+
+                displayAllParticipant();
+            }
+        }
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+
+    private void displayAllParticipant() {
+        JSONObject jsonObject = null;
+        ArrayList<HashMap<String, String>> arrayList = new ArrayList<HashMap<String, String>>();
+
+        try {
+            jsonObject = new JSONObject(JSON_STRING);
+            JSONArray jsonArray = jsonObject.getJSONArray(ConfigManager.TAG_JSON_ARRAY);
+            Log.d("Data_JSON_LIST: ", String.valueOf(jsonArray));
+
+
+            for (int i=0;i<jsonArray.length(); i++){
+                JSONObject object = jsonArray.getJSONObject(i);
+                String name = object.getString(ConfigManager.TAG_JSON_MNG_NAME);
+                String emp = object.getString(ConfigManager.TAG_JSON_MNG_NAME_EMP);
+
+                HashMap<String, String> customer = new HashMap<>();
+                customer.put(ConfigManager.TAG_JSON_MNG_NAME, name);
+                customer.put(ConfigManager.TAG_JSON_MNG_NAME_EMP, emp);
+
+                arrayList.add(customer);
+                Log.d("DataArr: ", String.valueOf(customer));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        ListAdapter adapter = new SimpleAdapter(
+                viewGroup.getContext(), arrayList, R.layout.lv_approval,
+                new String[] {ConfigManager.TAG_JSON_MNG_NAME, ConfigManager.TAG_JSON_MNG_NAME_EMP},
+                new int[] {R.id.lv_approve_nsb, R.id.lv_approve_emp}
+        );
+        Log.d("DataArray: ", String.valueOf(adapter));
+        listView.setAdapter(adapter);
+
+
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Bundle bundle = new Bundle();
+//                DetailCustomerFragment detailCustomerFragment = new DetailCustomerFragment();
+//                FragmentManager fragmentManager = getFragmentManager();
+//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//                fragmentTransaction.replace(R.id.framelayout,detailCustomerFragment);
+//
+//
+//                HashMap<String, String> map = (HashMap) adapterView.getItemAtPosition(i);
+//                String nsbid = map.get(ConfigCustomer.TAG_JSON_CST_ID).toString();
+//                Bundle args = new Bundle();
+//                args.putString("id", nsbid);
+//                detailCustomerFragment.setArguments(args);
+//
+//
+//
+//                Log.d("Par: ", String.valueOf(args));
+//                fragmentTransaction.commit();
+//            }
+//        });
+    }
+
+    public void callFragment(Fragment fragment) {
+        FragmentManager man = getActivity().getSupportFragmentManager();
+        FragmentTransaction trans = man.beginTransaction();
+        trans.setCustomAnimations(
+                android.R.anim.slide_in_left,
+                android.R.anim.slide_out_right
+        );
+        trans.replace(R.id.framelayout, fragment);
+        trans.addToBackStack(null);
+        trans.commit();
     }
 }
